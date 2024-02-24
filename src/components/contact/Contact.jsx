f381dcb8cbdce363f72d5869298efab01ad31769
Megan Go <62362053+megang33@users.noreply.github.com>
2024-02-24 09:45:21 -0800

    remove sendMessage + update contact info

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -1,28 +1,17 @@
 import React from 'react'
 import './contact.css'
-import{HiOutlineMail} from 'react-icons/hi'
+import { HiOutlineMail } from 'react-icons/hi'
 import { useRef } from 'react';
 import emailjs from 'emailjs-com'
-import SendMessage from '../sendMessage/sendMessage';
 
 
 export const Contact = () => {
   return (
     <section>
-      
-      <h2>Contact Us</h2>
-
       <div className="container contact__container">
-        <div className="contact__options">
-          <article className="contact__option">
-            <HiOutlineMail className='contact__option-icon'/>
-            <h4>Email</h4>
-            <h5>ewi.swe.ucla@gmail.com</h5>
-            <a href="mailto:ewi.swe.ucla@gmail.com">Send a message</a>
-          </article>
-        </div>
-        {/* END OF CONTACT OPTIONS */}
-        <SendMessage />
+        <h2 className="contact__header">Contact Us</h2>
+        <a href="mailto:advocacy.swe.ucla@gmail.com"><HiOutlineMail className='contact__option-icon' /></a>
+        <h3>advocacy.swe.ucla@gmail.com</h3>
       </div>
     </section>
   )
