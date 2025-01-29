a6dcf91fe7dd6b2c2350a5bedea81da6c155436a
cloyooni <146996021+cloyooni@users.noreply.github.com>
2025-01-29 00:01:54 -0800

    title and shooting star

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
 import './header.css'
 import Timer from '../timer/Timer'
-import LOGO from '../../assets/EPIC-logo.png'
+import LOGO from '../../assets/EPIC logo.png'
 import RINGS from '../../assets/rings.png'
+import STAR from '../../assets/shooting_star.png'
 
 const Header = () => {
   return (
@@ -12,6 +13,7 @@ const Header = () => {
         <div className="container header__container">
             <div className="landing_logo">
               <img src={LOGO} alt={'logo'}/>
+              <img src={STAR} alt={'star'}/>
             </div>
             <h3> Engineers for Professional Inclusion Conference </h3>
             <h3> April 25, 2024 5:30-8:30 PM PST <br/>@ Carnesale Commons </h3>
