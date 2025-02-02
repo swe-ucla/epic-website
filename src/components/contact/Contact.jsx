55da7df7480e4e5f89c985397f798006b29493fb
ashleyforrest00 <ashleyforrest00@gmail.com>
2025-02-02 09:25:57 -0800

    contact changes

--- src/components/contact/Contact.jsx
+++ src/components/contact/Contact.jsx
@@ -13,16 +13,16 @@ export const Contact = () => {
 
         <svg width="0" height="0">
           <linearGradient id="blue-gradient" x1="0%" y1="0%" x2="0%" y2="100%">
-          <stop offset="0%" stop-color="rgba(251, 7, 217, 1)"/>
-          <stop offset="13%" stop-color="rgba(255, 0, 0, 1)"/>
-          <stop offset="24%" stop-color="rgba(255, 154, 0, 1)"/>
-          <stop offset="35%" stop-color="rgba(208, 222, 33, 1)"/>
-          <stop offset="46%" stop-color="rgba(79, 220, 74, 1)"/>
-          <stop offset="57%" stop-color="rgba(63, 218, 216, 1)"/>
-          <stop offset="68%" stop-color="rgba(47, 201, 226, 1)"/>
-          <stop offset="79%" stop-color="rgba(28, 127, 238, 1)"/>
-          <stop offset="90%" stop-color="rgba(95, 21, 242, 1)"/>
-          <stop offset="100%" stop-color="rgba(186, 12, 248, 1)"/>
+          <stop offset="0%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="13%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="24%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="35%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="46%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="57%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="68%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="79%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="90%" stop-color="rgb(214, 126, 246)"/>
+          <stop offset="100%" stop-color="rgb(214, 126, 246)"/>
           </linearGradient>
         </svg>
 
