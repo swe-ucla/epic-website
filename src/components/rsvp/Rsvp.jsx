462ee2333b0d1dbfe09d2531c333a2730d8903b1
ashleyforrest00 <ashleyforrest00@gmail.com>
2025-02-02 09:48:17 -0800

    changes to schedule, speakers (bios/photos), and rsvp

--- src/components/rsvp/Rsvp.jsx
+++ src/components/rsvp/Rsvp.jsx
@@ -1,21 +1,19 @@
 import React from 'react'
 import './rsvp.css'
-import LINES from '../../assets/lines.png'
 
 
 const Rsvp = () => {
   return (
       <section>
         <div id='rsvp'><br/><br/><br/></div>
-        <img className='divider-lines-rsvp' src={LINES}/>
         <div className="container attend__container">
           <div className='rsvp_content'>
             <div className='rsvp_column'>
               <iframe className='rsvp_embed' src="https://docs.google.com/forms/d/e/1FAIpQLSevweJQfFfKqbf9c8hu0PcyzBJquY-Xa3Z1b2Qh4od-rO1lfA/viewform?embedded=true" width="100%" height="500vw" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
             </div>
             <div className='rsvp_column'>
-              <h2 className="rsvp_title">register for EPIC 2024</h2>
-              <p> EPIC 2024 will be taking place on Thursday, April 25, 2024, from 5:30 PM to 8:30 PM PST at Carnesale Commons. Dress code: business casual </p>
+              <h2 className="rsvp_title">register for EPIC 2025</h2>
+              <p> EPIC 2025 will be taking place on Wednesday, April 9, 2024, from 5:00 PM to 8:30 PM PST at Pauley Pavilion Clubhouse [may update] </p>
               <p> Registration is free and open to all UCLA students. </p>
               <a className="rsvp_link" href="https://forms.gle/CUrFzmEeeFpCSdKJ6" target='_blank'> RSVP </a>
             </div>
--- src/components/speakers/Speakers.jsx
+++ src/components/speakers/Speakers.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './speakers.css'
-import Tinsley from '../../assets/TAyonnah Pic.png'
+import Speaker2 from '../../assets/filler_pfp.png'
 import Cisneros from '../../assets/MCisneros Pic.png'
-import Bao from '../../assets/LirongBao.jpeg'
-import Kearns from '../../assets/Kailin Kearns.JPEG'
+import Speaker1 from '../../assets/filler_pfp.png'
+import Speaker3 from '../../assets/filler_pfp.png'
 import Pasman from '../../assets/ReneePasman.png'
 import Santos from '../../assets/VeronicaSantos.jpg'
 import LINES from '../../assets/lines.png'
@@ -14,50 +14,47 @@ const Speakers = () => {
     <section>
       <div id='speakers'><br/></div>
       <div className="container speaker__content"> 
-            <h2>speakers</h2>
-            <img className='divider-lines' src={LINES}/>
+            <h2 style={{padding: '35px'}}>speakers</h2>
           <div class="speaker_row">
             <div class="speaker_column_image">
-              <img className='speaker-img' src={Bao} alt={'Lirong Bao photo'} style={{borderRadius: '10%' }}/>
+              <img className='speaker-img' src={Speaker1} alt={'Lirong Bao photo'} style={{borderRadius: '10%' }}/>
             </div>
             <div class="speaker_column_text">
-              <p class="title">Lirong Bao</p>
-              <p class="subtitle">Global Science and Technology Director</p>
+              <p class="title">Kim Jones</p>
+              <p class="subtitle">Title</p>
               <p>
-                Lirong Bao is the Global Science and Technology Director for aerospace materials at PPG Industries, a leading supplier of innovative paints, coatings, and specialty materials. PPG’s aerospace business provides differentiated materials, products, and services for the global aerospace market. Lirong and her team focus on product and technology development for aerospace coatings, sealants, adhesives and functional materials. They serve the diverse needs of aerospace industry and collaborate closely with customers and partners to bring novel products to market. Lirong is based in Burbank, California.
-                <br/>
-                Lirong holds a Ph.D. degree in Macromolecular Science and Engineering from the University of Michigan and a B.S. degree in Materials Science from Fudan University in China. Before joining PPG in 2021, she held various technical and leadership roles in Henkel, AkzoNobel and National Starch and Chemical Company.
+              Speaker bio Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus volutpat lacus sit amet enim feugiat, et imperdiet justo convallis. Mauris quis vestibulum nisl. Aenean sagittis accumsan orci, quis eleifend ligula ultricies in. Fusce fringilla ullamcorper purus, non tincidunt neque vehicula nec. Maecenas tristique metus nec justo accumsan gravida. Praesent hendrerit sem diam, in consequat risus auctor vitae.
               </p>
             </div>
           </div>
 
           <div class="speaker_row backward">
             <div class="speaker_column_text">
-              <p class="title">Ayonnah Tinsley</p>
-              <p class="subtitle">STEM Content Creator</p>
+              <p class="title">Samantha Combs</p>
+              <p class="subtitle">Title</p>
               <p>
-              Ayonnah Tinsley is a Junior at the University of Southern California studying Arts, Technology, and the Business of Innovation at the Iovine Young Academy, with a minor in Sports Media Industries. Although her studies sound like a mouthful, she incorporates or passion for technology, sports, and art through her social media series called, “STEMix Series”, and through her children’s book she wrote, illustrated, and published titled, “This Is What an Engineer Looks Like”. She always had a passion for STEM education, but during her Junior year she realized her passion for the sports too! Her goal is to highlight the STEM behind sports, and give a spotlight to athletes that are a part of both worlds. Not only is she interested in the industry, but loves playing sports too! From ultimate frisbee to rollerskating, or even learning how to boulder.
+              Speaker bio Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus volutpat lacus sit amet enim feugiat, et imperdiet justo convallis. Mauris quis vestibulum nisl. Aenean sagittis accumsan orci, quis eleifend ligula ultricies in. Fusce fringilla ullamcorper purus, non tincidunt neque vehicula nec. Maecenas tristique metus nec justo accumsan gravida. Praesent hendrerit sem diam, in consequat risus auctor vitae.
               </p>
             </div>
             <div class="speaker_column_image">
-              <img className='speaker-img' src={Tinsley} alt={'Ayonnah Tinsley photo'} style={{  borderRadius: '10%' }}/>
+              <img className='speaker-img' src={Speaker2} alt={'Ayonnah Tinsley photo'} style={{  borderRadius: '10%' }}/>
             </div>
           </div>
 
           <div class="speaker_row">
             <div class="speaker_column_image">
-              <img className='speaker-img' src={Kearns} alt={'Kailin Kearns photo'} style={{borderRadius: '10%' }}/>
+              <img className='speaker-img' src={Speaker3} alt={'Kailin Kearns photo'} style={{borderRadius: '10%' }}/>
             </div>
             <div class="speaker_column_text">
-              <p class="title">Kaitlin Kearns</p>
-              <p class="subtitle">Engineering Consulting Firm Recruiter</p>
+              <p class="title">Elizabeth Krider</p>
+              <p class="subtitle">Title</p>
               <p>
-              Kaitlin Kearns is a Recruiter at Second Order Effects, an engineering consulting firm that delivers novel solutions to customers in multiple industries (aerospace, medical devices, renewable energy, robotics, etc.). She discovered her passion for Recruiting during her time at UCLA, where she majored in Economics and graduated in June of 2022. Throughout her time so far at SOE, Kaitlin has taken ownership over the hiring process for both engineering and non-engineering roles and is responsible for adding talented individuals to the team. Her most notable achievements include hiring Engineering Managers for the Electrical and Mechanical teams and sourcing SOE’s current Director of Operations and Information Technology. 
+              Speaker bio Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus volutpat lacus sit amet enim feugiat, et imperdiet justo convallis. Mauris quis vestibulum nisl. Aenean sagittis accumsan orci, quis eleifend ligula ultricies in. Fusce fringilla ullamcorper purus, non tincidunt neque vehicula nec. Maecenas tristique metus nec justo accumsan gravida. Praesent hendrerit sem diam, in consequat risus auctor vitae.
               </p>
             </div>
           </div>
 
-          <div class="speaker_row backward">
+          {/* <div class="speaker_row backward">
             <div class="speaker_column_text">
               <p class="title">Veronica Santos</p>
               <p class="subtitle">UCLA Samueli Associate Dean</p>
@@ -68,7 +65,7 @@ const Speakers = () => {
             <div class="speaker_column_image">
               <img className='speaker-img' src={Santos} alt={'Veronica Santos photo'} style={{ borderRadius: '10%' }}/>
             </div>
-          </div>
+          </div> */}
 
           {/* <div class="speaker_row">
             <div class="speaker_column_image">
