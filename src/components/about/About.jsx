fc4cfabc2f1a5e2058a0cfb3c0eeb8f67d52e29a
ashleyforrest00 <ashleyforrest00@gmail.com>
2025-02-02 09:22:08 -0800

    about changes

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import './about.css'
-import EPIC_END from '../../assets/epic_end.jpg'
-import EPIC_EVENT from '../../assets/epic_event.jpg'
-import EPIC_SIGN from '../../assets/epic_sign.jpg'
-import LINES from '../../assets/lines.png'
+import EPIC_END from '../../assets/Group 6.png'
+import EPIC_EVENT from '../../assets/Group 19.png'
+import EPIC_SIGN from '../../assets/Group 20 (1).png'
 
 
 const About = () => {
@@ -11,15 +10,13 @@ const About = () => {
     <section>
       <div id='about'><br/></div>
       <div className="container about__content"> 
-            <h2>about</h2>
-            <img className='divider-lines' src={LINES}/>
+            <h2 style={{padding: '25px'}}>about</h2>
 
         <div class="about_table">
           <div class="about_row">
             <div class="about_column">
               <p>
-                The Engineers for Professional Inclusion Conference (EPIC) is the Society of Women Engineers’ Advocacy committee’s flagship event, engaging students, company representatives, and academics from a wide range of backgrounds in an open conversation about what it means to be inclusive in a professional environment.
-              </p>
+              The Engineers for Professional Inclusion Conference (EPIC) is the Society of Women Engineers’ Advocacy committee’s flagship event, engaging students, company representatives, and academics from a wide range of backgrounds in an open conversation about what it means to be inclusive in a professional environment.</p>
             </div>
             <div class="about_column">
               <img src={EPIC_EVENT} alt={'Attendees sitting at EPIC event'} style={{  borderRadius: '10%' }}/>
@@ -32,16 +29,14 @@ const About = () => {
             </div>
             <div class="about_column">
               <p>
-              Our goal for EPIC is to facilitate meaningful conversations about diversity between a wide range of students and professionals and help participants brainstorm solutions that they can apply at UCLA and their workplaces. Overall, we hope that both students and professionals walk away from this event feeling more motivated and optimistic about fixing the lack of diversity in engineering, and realize they have a community on campus that cares. Additionally, we hope that these conversations will give them the tools to start and lead more conversations with others in an effort to heighten awareness surrounding obstacles that minorities in STEM fields face.
-              </p>
+              Our goal for EPIC is to facilitate meaningful conversations about diversity between a wide range of students and professionals and help participants brainstorm solutions that they can apply at UCLA and their workplaces. Overall, we hope that both students and professionals walk away from this event feeling more motivated and optimistic about fixing the lack of diversity in engineering, and realize they have a community on campus that cares. Additionally, we hope that these conversations will give them the tools to start and lead more conversations with others in an effort to heighten awareness surrounding obstacles that minorities in STEM fields face.</p>
             </div>
           </div>
 
           <div class="about_row">
             <div class="about_column">
               <p>
-              EPIC 2024 will be held at UCLA’s Carnesale Commons on the evening of Thursday, April 25th. In our commitment to fostering inclusivity, we strive to include as many voices as possible in the conversation about diversity and inclusion in engineering and thus welcome attendees from all backgrounds and identities to join us in this dialogue, recognizing that diverse perspectives enrich our collective understanding and drive meaningful change. After all, we stand for inclusion here.
-              </p>
+              EPIC 2025 will be held at [tbd] on the evening of Wednesday, April 9th. In our commitment to fostering inclusivity, we strive to include as many voices as possible in the conversation about diversity and inclusion in engineering and thus welcome attendees from all backgrounds and identities to join us in this dialogue, recognizing that diverse perspectives enrich our collective understanding and drive meaningful change. After all, we stand for inclusion here.</p>
             </div>
             <div class="about_column">
             <img src={EPIC_END} alt={'EPIC attendees smiling at end of EPIC event'} style={{ borderRadius: '10%' }}/>
