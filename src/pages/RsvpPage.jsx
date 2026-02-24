a60b58d46ae50abd816dcaa4f193645e51921027
anyachen27 <150403358+anyachen27@users.noreply.github.com>
2026-02-24 11:04:32 -0800

    Add Footer component to multiple pages: Speakers, Schedule, Rsvp, and About

--- src/pages/RsvpPage.jsx
+++ src/pages/RsvpPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbar from '../components/navbar/navbar'
 import Rsvp from '../components/rsvp/Rsvp'
+import Footer from '../components/footer/Footer'
 
 const RsvpPage = () => {
   return (
     <div className='rsvp-page'>
       <Navbar />
       <Rsvp />
+      <Footer />
     </div>
   )
 }
